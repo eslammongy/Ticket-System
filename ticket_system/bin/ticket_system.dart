f9616b59afd15f7559546cfde9fd6de96ad6ba57
eslam <eslammongy995@gmail.com>
2023-01-25 13:04:56 +0200

    initial commit

---
+++ ticket_system/bin/ticket_system.dart
@@ -0,0 +1,5 @@
+import 'package:ticket_system/ticket_system.dart' as ticket_system;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ticket_system.calculate()}!');
+}
---
+++ ticket_system/test/ticket_system_test.dart
@@ -0,0 +1,8 @@
+import 'package:ticket_system/ticket_system.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
